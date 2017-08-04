352a81948ee2b35199d6873dfcf5a5836eb55dcd
mateogolf <rodriguezml11@mail.wlu.edu>
2017-08-04 19:21:11 -0400

    Validation Practice

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect, request, session, flash
 app = Flask(__name__)
+app.secret_key = 'KeepItSecretKeepItSafe'
 
 
 @app.route('/')
@@ -9,16 +10,41 @@ def whatName():
 
 @app.route('/result', methods=['POST'])
 def result():
-    name = request.form["name"]
-    print name
+    #rest of assigned values
     location = request.form['location']
-    print location
     fav_lang = request.form['favorite']
-    print fav_lang
-    comments = request.form['comments']
-    print comments
-    # , location=location,fav_lang=fav_lang,comments=comments)
-    return render_template('result.html', name=name, location=location, fav_lang=fav_lang, comments=comments)
+    nameMessage = "Name cannot be empty!"
+    emptyComment = "Comments cannot be empty!"
+    bigComments = "Comments is too long!"
+    #validation conditionals
+    if len(request.form['name']) < 1 and (len(request.form['comments']) < 1 or len(request.form['comments']) > 120):
+        session['name'] = ""
+        session['comments'] = ""
+        noName = nameMessage
+        if len(request.form['comments']) < 1:
+            noComments = emptyComment
+        else:
+            noComments = bigComments
+        return render_template('index.html', noName=noName, noComments=noComments)
+    elif len(request.form['name']) < 1:
+        noName = nameMessage
+        session['name'] = ""
+        session['comments'] = request.form['comments']
+        return render_template('index.html',noName=noName)
+    elif len(request.form['comments']) < 1:
+        noComments = emptyComment
+        session['comments'] = ""
+        session['name'] = request.form["name"]
+        return render_template('index.html', noComments=noComments)
+    elif len(request.form['comments']) > 120:
+        noComments = bigComments
+        session['comments'] = request.form['comments']
+        session['name'] = request.form["name"]
+        return render_template('index.html', noComments=noComments)
+    else:
+        session['name'] = request.form["name"]
+        session['comments'] = request.form['comments']
+    return render_template('result.html', location=location, fav_lang=fav_lang)
 
 
 app.run(debug=True)
